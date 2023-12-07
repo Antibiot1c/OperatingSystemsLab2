c0f9f55d04a882f0a8423e3758f581e31fa5a935
Pavlo Velychko <112926321+Antibiot1c@users.noreply.github.com>
2023-12-07 23:01:05 +0200

    Update SchedulingAlgorithm.java

--- SchedulingAlgorithm.java
+++ SchedulingAlgorithm.java
@@ -8,57 +8,58 @@ public class SchedulingAlgorithm {
   private static double[] processWeights;
 
   public static Results Run(int runtime, Vector processVector, Results result) {
-    int i = 0;
-    int comptime = 0;
-    int currentProcess = 0;
-    int size = processVector.size();
-    int completed = 0;
-    String resultsFile = "Summary-Processes";
+        int i = 0;
+        int comptime = 0;
+        int currentProcess = 0;
+        int size = processVector.size();
+        int completed = 0;
+        String resultsFile = "Summary-Processes";
 
-    result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "Lottery Scheduling";
+        result.schedulingType = "Batch (Nonpreemptive)";
+        result.schedulingName = "Lottery Scheduling";
 
-    initializeProcessWeights(processVector);
+        initializeProcessWeights(processVector);
 
-    try {
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
-      sProcess process = (sProcess) processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+        try {
+            PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
+            sProcess process = (sProcess) processVector.elementAt(currentProcess);
+            out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
 
-      while (comptime < runtime) {
-        if (process.cpudone == process.cputime) {
-          completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+            while (comptime < runtime) {
+                if (process.cpudone == process.cputime) {
+                    completed++;
+                    out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
 
-          if (completed == size) {
-            result.compuTime = comptime;
+                    if (completed == size) {
+                        result.compuTime = comptime;
+                        out.close();
+                        return result;
+                    }
+
+                    currentProcess = getRandomWeightedIndex(processWeights);
+                    process = (sProcess) processVector.elementAt(currentProcess);
+
+                    out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+                }
+
+                process.cpudone++;
+
+                if (process.ioblocking > 0) {
+                    process.ionext++;
+                }
+
+                comptime++;
+
+                System.out.println("Current Time: " + comptime + " ms");
+            }
             out.close();
-            return result;
-          }
-
-          currentProcess = getRandomWeightedIndex(processWeights);
-          process = (sProcess) processVector.elementAt(currentProcess);
-
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-
-        process.cpudone++;
-
-        if (process.ioblocking > 0) {
-          process.ionext++;
-        }
-
-        comptime++;
-      }
-      out.close();
-    } catch (IOException e) {
-      e.printStackTrace();
+        result.compuTime = comptime;
+        return result;
     }
-
-    result.compuTime = comptime;
-    return result;
-  }
 
   private static void initializeProcessWeights(Vector processVector) {
     int size = processVector.size();
